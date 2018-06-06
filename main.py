6d124b46f9847976059eb90d480438b3aa5dfec2
trystan.may <trystan.may@wright.edu>
2018-06-06 18:44:44 -0400

    Added training and testing to main.py

--- main.py
+++ main.py
@@ -1,5 +1,63 @@
+from torchvision import datasets
+from torchvision import transforms
+
+import torch.optim
+from torch import nn
+import torch.utils.data
+
+import network
+
+
 def main ():
-	print ( "Hello World!" )
+	net = network.Network ()
+
+	transformations = transforms.Compose ([
+		transforms.ToTensor()
+	])
+	trainingDataset = datasets.MNIST ( root = 'data/train', transform = transformations,
+	                                   download = True )
+	testingDataset = datasets.MNIST ( root = 'data/test', transform = transformations,
+	                                  train = False, download = True )
+
+	trainingDataLoader = torch.utils.data.DataLoader ( trainingDataset,
+	                                                   batch_size = 32,
+	                                                   shuffle = True )
+	testingDataLoader = torch.utils.data.DataLoader ( testingDataset,
+	                                                  batch_size = len ( testingDataset ) )
+
+	optimizer = torch.optim.SGD ( net.parameters (), lr = 1e-02 )
+
+	lossFunction = nn.CrossEntropyLoss ()
+
+	print ( "Training!" )
+	net.train ()
+	for epoch in range ( 0, 10 ):
+		print ( "Epoch {}".format ( epoch ) )
+		for batchNum, (data, target) in enumerate ( trainingDataLoader ):
+
+			optimizer.zero_grad ()
+
+			output = net ( data )
+
+			loss = lossFunction ( output, target )
+			loss.backward ()
+
+			optimizer.step ()
+
+			if batchNum % 50 == 0:
+				print ( "Training loss at epoch {} batch number {}: {}".format ( epoch, batchNum,
+				                                                                 loss ) )
+
+	print ( "Testing!" )
+	net.eval ()
+	correct = 0
+	for _, (data, target) in enumerate ( testingDataLoader ):
+		output = net ( data )
+		loss = lossFunction ( output, target )
+
+		pred = output.max ( 1, keepdim = True )[1]  # get the index of the max log-probability
+		correct += pred.eq ( target.view_as ( pred ) ).sum ().item ()
+	print ( "Number of correctness is {}".format ( correct ) )
 
 
 if __name__ == '__main__':
